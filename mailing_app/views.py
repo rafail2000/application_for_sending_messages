3535a6e0436dc8fb5ae7255128b39ff4a81659f0
ChistyakovaVM <rafail.shiryaev.2015@mail.ru>
2026-01-19 16:56:27 +0300

    add views

--- mailing_app/views.py
+++ mailing_app/views.py
@@ -1,3 +1,54 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, ListView, DetailView, UpdateView
 
-# Create your views here.
+from mailing_app.models import Message
+
+
+class MessageCreateView(CreateView):
+    """
+    Курсор для создания сообщений
+    """
+
+    model = Message
+    template_name = 'mailing_app/message_form.html'
+    success_url = reverse_lazy('mailing_app/message_list.html')
+
+
+class MessageListView(ListView):
+    """
+    Курсор для просмотра списка сообщений
+    """
+
+    model = Message
+    template_name = 'mailing_app/message_list.html'
+    context_object_name = 'message'
+
+
+class MessageDetailView(DetailView):
+    """
+    Курсор для просмотра сообщения
+    """
+
+    model = Message
+    template_name = 'mailing_app/message_detail.html'
+    context_object_name = 'message'
+
+
+class MessageUpdateView(UpdateView):
+    """
+    Курсор для обновления сообщения
+    """
+
+    model = Message
+    template_name = 'mailing_app/message_forml.html'
+    success_url = reverse_lazy('mailing_app/message_list.html')
+
+
+class MessageDeleteView(DetailView):
+    """
+    Курсор для удаления сообщения
+    """
+
+    model = Message
+    template_name = 'mailing_app/message_confirm_delete.html'
+    success_url = reverse_lazy('mailing_app/message_list.html')
