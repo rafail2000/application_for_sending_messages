a30ccd293d8b49b1b88f6889f9988e0a6764464e
ChistyakovaVM <rafail.shiryaev.2015@mail.ru>
2026-01-20 10:52:43 +0300

    add admin

--- mailing_app/admin.py
+++ mailing_app/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
-# Register your models here.
+from mailing_app.models import MailingRecipient, Message, Mailing, MailingAttempts
+
+
+@admin.register(MailingRecipient)
+class MailingRecipientAdmin(admin.ModelAdmin):
+    list_display = ('id', 'email', 'initials', 'comment',)
+    list_filter = ('initials',)
+    search_fields = ('name', 'description',)
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('id', 'letter_subject', 'letter_body',)
+    list_filter = ('id',)
+    search_fields = ('letter_subject', 'letter_body',)
+
+@admin.register(Mailing)
+class MailingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'start_time', 'end_time', 'status', 'message', 'recipients',)
+    list_filter = ('id',)
+    search_fields = ('status', 'message', 'recipients')
+
+@admin.register(MailingAttempts)
+class MailingAttemptsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'attempt_time', 'status', 'server_response', 'mailing',)
+    list_filter = ('id',)
+    search_fields = ('status', 'server_response', 'mailing')
